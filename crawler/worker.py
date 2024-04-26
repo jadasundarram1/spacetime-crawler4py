d5654c138d2d8278aa6926897aef2ff5236da02f
Kimberly Marin <marink2@uci.edu>
2024-04-26 16:32:02 -0700

    Added subdomain token function list to meet requirements

--- crawler/worker.py
+++ crawler/worker.py
@@ -5,13 +5,26 @@ from utils.download import download
 from utils import get_logger
 import scraper
 import time
-from urllib.parse import urldefrag
+from urllib.parse import urldefrag, urlparse
+
+def get_subdomain(url):
+    parsed_url = urlparse(url)
+    domain_parts = parsed_url.netloc.split('.')
+    if len(domain_parts) > 2:
+        return ','.join(domain_parts[:-2])
+    return ''
+
+def print_subdomain_counts(subdomain_counts):
+    sorted_subdomains = sorted(subdomain_counts.keys())
+    for subdomain in sorted_subdomains:
+        print(f"{subdomain}.ics.uci.edu, {len(subdomain_counts[subdomain])}")
 
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
+        self.subdomain_counts = {}
         # basic check for requests in scraper
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
@@ -40,6 +53,14 @@ class Worker(Thread):
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             unique_urls.add(urldefrag(tbd_url)[0])
+
+            # Grab subdomains
+            subdomain = get_subdomain(tbd_url)
+            if subdomain not in self.subdomain_counts:
+                self.subdomain_counts[subdomain] = set()
+            self.subdomain_counts[subdomain].add(urldefrag(tbd_url)[0])
+
             time.sleep(self.config.time_delay)
         print(f"Number of unique pages found: {len(unique_urls)}")
         print (f"Longest page found was {scraper.get_max_length_url()[1]} with {scraper.get_max_length_url()[0]} words.")
+        print_subdomain_counts(self.subdomain_counts)

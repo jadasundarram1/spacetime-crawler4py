7185d6422b7071989b72a54587f226f41e2757cb
Sean Bochaton <70482790+sbochato@users.noreply.github.com>
2024-05-02 15:38:58 -0700

    Update scraper.py to add 50 most common words

--- scraper.py
+++ scraper.py
@@ -112,6 +112,23 @@ def extract_next_links(url, resp, max_redirects = 10):
         #get length of url content to add to dict
         len_content = len(soup.get_text().split())
         url_content_length[len_content] = url
+
+        current_word_count = 0
+        max_word_link = ("", 0)
+        common_words = defaultdict(int)
+        stopwords = {'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"}
+        for line in soup.stripped_strings: #stripped_strings is an iterator that gets all the lines in the document (better than get_text which loads all the data in one variable at once)
+            lowerLine = line.lower()
+            for word in re.findall(r'[a-zA-Z0-9]+', lowerLine): #alphanumeric words
+                if word not in stopwords and ((word.isdigit() and len(word) >= 3) or (not word.isdigit() and len(word) > 1)): #filtering stopwords and small numbers/words
+                    common_words[word] += 1
+                    current_word_count += 1
+
+            if currrent_word_count > maxWordLink[1]: #keeping track of url with most words
+                max_word_link = (url, current_word_count) #storing in a tuple with the url associated with most words
+            else:
+                isSitemap = True
+                soup = BeautifulSoup(resp.raw_response.content, 'lxml-xml') #have to change the soup object to parse XML sites for sitemaps
         
         # Find all hyperlinks
         links = soup.findAll('a')
